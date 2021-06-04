a16001f9c76fcb1d2cdaf537bf3545fe07837a40
Francesco Favero <francesco.favero@bric.ku.dk>
2021-06-04 10:04:55 +0200

    Add percentage, print nicer info, bundle the amount of sells orders (if more than 1), to sell together

--- bots/a_grid_bot.py
+++ bots/a_grid_bot.py
@@ -9,7 +9,7 @@ INTERVAL_BIG = "1h"
 INTERVAL_SMALL = "1m"
 
 SYMBOLS = [
-   "ETHUSDT", "BTCUSDT", "ADAUSDT"]
+   "BTCUSDT", "ETHUSDT", "ADAUSDT"]
 
 def initialize(state):
     state.buy_orders = {}
@@ -22,7 +22,7 @@ def initialize(state):
         "max_hold_hours": 23,
         "max_open_orders": 4,
         "atr_grid_step": 1,
-        "percent_grid_step": None,
+        "percent_grid_step": 0.015,
         "value_per_order": 20,
         "atr_period": 12}
 
@@ -119,7 +119,7 @@ def handler_small_fun(state, data):
                 "At least one of the two options "
                 "atr_grid_step or percent_grid_step need to be set"))
         percent_grid_step = atr_to_percent(
-            float(big_candle_current_price), float(atr), atr_grid_step, False)
+            float(fix_value), float(atr), atr_grid_step, False)
     n_step_from_origin = int(get_number_of_steps(
         current_price, fix_value, percent_grid_step))
     level_price = get_price_of_closest_step(
@@ -128,7 +128,7 @@ def handler_small_fun(state, data):
     #    symbol, current_price, n_step_from_origin, level_price))
 
     with PlotScope.root(symbol):
-        #for i in range(n_step_from_origin - 1, n_step_from_origin + 1):
+        for i in range(n_step_from_origin - 1, n_step_from_origin + 1):
             i = n_step_from_origin
             level_i_price = get_price_of_closest_step(
                 fix_value, i, percent_grid_step)
@@ -142,7 +142,7 @@ def handler_small_fun(state, data):
     bought_levels = [x["level"] for x in buy_orders]
     if current_high >= level_price and current_price <= level_price:
         if len(buy_orders) < max_open_orders and n_step_from_origin not in bought_levels:
-            print("I can buy!")
+            print("%s Buying at level %i" % (symbol, n_step_from_origin))
             buy_order = order_market_value(symbol, value = value_per_order)
             state.buy_orders[symbol].append({
                 "level": n_step_from_origin,
@@ -151,29 +151,38 @@ def handler_small_fun(state, data):
             })
     elif current_low <= level_price and current_price >= level_price:
         orders_left = []
+        sell_amount = 0
         for order in buy_orders:
             order_level = order["level"]
             if order_level < n_step_from_origin:
-                amount = float(order["order"].quantity)
-                order_market_amount(symbol, -1 * subtract_order_fees(amount))
+                sell_amount += float(order["order"].quantity)
+                print("%s Sell at level %i quantity bought at level %i" % (
+                    symbol, n_step_from_origin, order_level))
             else:
                 orders_left.append(order)
+        if sell_amount > 0:
+            print("%s %i orders left to sell" % (symbol, len(orders_left)))
+            order_market_amount(symbol, -1 * subtract_order_fees(sell_amount))
+
         buy_orders = orders_left
     # sell old orders
     if max_hold_hours:
         orders_left = []
+        sell_amount = 0
         for order in buy_orders:
             order_time = datetime.fromtimestamp(order["time"] / 1000)
             this_time = datetime.fromtimestamp(current_time / 1000)
-
             hold_hours = (this_time - order_time).seconds / 3600
             if hold_hours > max_hold_hours:
-                amount = float(order["order"].quantity)
-                order_market_amount(symbol, -1 * subtract_order_fees(amount))
+                sell_amount += float(order["order"].quantity)
+                print("%s Sell at level %i: hours holding %s (max %s)" % (
+                    symbol, n_step_from_origin, hold_hours, max_hold_hours))
             else:
                 orders_left.append(order)
+        if sell_amount > 0:
+            print("%s %i orders left to sell" % (symbol, len(orders_left)))
+            order_market_amount(symbol, -1 * subtract_order_fees(sell_amount))                
         buy_orders = orders_left
-
     state.buy_orders[symbol] = buy_orders
 
 def get_number_of_steps(current_price, init_price, step_size):
